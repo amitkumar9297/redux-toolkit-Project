77bd4505e9e4dd0d539802a6f549b6e3d71c8e06
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-14 16:57:28 +0530

    fetch data from store

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { add } from '../store/cartSlice'
+
+import { fetchProducts } from '../store/productSlice';
 
 const Products = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+    const { data: products, status } = useSelector((state) => state.product)
+    // const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const data = await res.json();
-            // console.log(data)
-            setProducts(data);
-        }
+        dispatch(fetchProducts())
+        // const fetchProducts = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products');
+        //     const data = await res.json();
+        //     // console.log(data)
+        //     setProducts(data);
+        // }
 
-        fetchProducts()
-    }, [])
+        // fetchProducts()
+    }, []);
+
+    const handleAdd = (product) => {
+        // yeha se product ko redux store mein store krna hai
+        // uske liye application mein se action dispatch krte hai
+
+        dispatch(add(product))
+    }
     return (
         <div className='productsWrapper'>
             {
@@ -21,7 +35,7 @@ const Products = () => {
                         <img src={product.image} alt='img' />
                         <h4>{product.title}</h4>
                         <h5>{product.price}</h5>
-                        <button className='btn'>Add to Cart</button>
+                        <button onClick={() => handleAdd(product)} className='btn'>Add to Cart</button>
                     </div>
                 ))
             }
