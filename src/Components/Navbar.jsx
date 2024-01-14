9fdaef836397e83a84636173423c14216a051b11
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-14 16:58:12 +0530

    update cart using redux

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+    const items = useSelector((state) => state.cart);// subscribe krna hota hai taki agr state update ho toh wo reflect ho . jaise hmm ne cart ko select kiya hai
     return (
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
             <span className='logo'>STORE</span>
@@ -9,7 +11,7 @@ const Navbar = () => {
                 <Link className='navLink' to='/'>Home</Link>
                 <Link className='navLink' to={'/cart'}>Cart</Link>
                 <span className='cartCount'>
-                    cart Item : 0
+                    cart Item : {items.length}
                 </span>
             </div>
         </div>
