987b10d9a6c61f3b51209ec52c310e975ad41673
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-14 17:37:36 +0530

    another way to write thunks

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,4 +1,4 @@
-const { createSlice } = require('@reduxjs/toolkit')
+const { createSlice, createAsyncThunk } = require('@reduxjs/toolkit')
 
 // const initialState = [];  //sabse pehle initial state declare krnahai
 
@@ -17,20 +17,33 @@ const productSlice = createSlice({
         status: STATUSES.IDLE, //idle,loading,error
     },
     reducers: {
-        setProducts(state, action) {
-            /*
-            Don't do this
-            const res = await fetch('https://fakestoreapi.com/products');
-            
-            because reducer synchronously call hoti hai
-            aur yeh pure function hote hai jiske koi side effect nahi hote
-            esa krne se side effect ho skta hai
-             */
-            state.data = action.payload;
-        },
-        setStatus(state, action) {
-            state.status = action.payload
-        }
+        // setProducts(state, action) {
+        //     /*
+        //     Don't do this
+        //     const res = await fetch('https://fakestoreapi.com/products');
+
+        //     because reducer synchronously call hoti hai
+        //     aur yeh pure function hote hai jiske koi side effect nahi hote
+        //     esa krne se side effect ho skta hai
+        //      */
+        //     state.data = action.payload;
+        // },
+        // setStatus(state, action) {
+        //     state.status = action.payload
+        // }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProducts.pending, (state, action) => {
+                state.status = STATUSES.LOADING;
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.data = action.payload
+                state.status = STATUSES.IDLE;
+            })
+            .addCase(fetchProducts.rejected, (state, action) => {
+                state.status = STATUSES.ERROR;
+            })
     }
 })
 
@@ -38,20 +51,33 @@ export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
 
-export function fetchProducts() {
-    return async function fetchProductThunk(dispatch, getState) {
-        dispatch(setStatus(STATUSES.LOADING))
-        try {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const data = await res.json();
-            dispatch(setProducts(data));
-            dispatch(setStatus(STATUSES.IDLE))
-        } catch (err) {
-            console.log(err);
-            dispatch(setStatus(STATUSES.ERROR))
-        }
-    }
-}
+export const fetchProducts = createAsyncThunk('products/fetch', async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const data = await res.json();
+    return data
+})
+
+// createAsyncThunk create to better error handling
+
+
+
+// THUNKS
+
+
+// export function fetchProducts() {
+//     return async function fetchProductThunk(dispatch, getState) {
+//         dispatch(setStatus(STATUSES.LOADING))
+//         try {
+//             const res = await fetch('https://fakestoreapi.com/products');
+//             const data = await res.json();
+//             dispatch(setProducts(data));
+//             dispatch(setStatus(STATUSES.IDLE))
+//         } catch (err) {
+//             console.log(err);
+//             dispatch(setStatus(STATUSES.ERROR))
+//         }
+//     }
+// }
 
 /* 
 thunks middleware hai redux ke andar
