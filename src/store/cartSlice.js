db2b691863a4e6c12c24a16fcb0defdbb7d32f21
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-14 13:15:48 +0530

    react-redux and reduxjs/toolkit implemented

---
+++ src/store/cartSlice.js
@@ -0,0 +1,21 @@
+const { createSlice } = require('@reduxjs/toolkit')
+
+const initialState = [];  //sabse pehle initial state declare krnahai
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        add(state, action) {
+            //in previous version of redux we cann't directly mutate the state, so we return a new state . but in latest version of redux we directly mutate the state because its a createSlice feature. bydesfault ceate slice do same thing as previous version of react
+            // create slice is a method of reduxjs/toolkit
+            state.push(action.payload)
+        },
+        remove(state, action) {
+            state = state.filter(item => item.id !== action.payload)
+        }
+    }
+})
+
+export const { add, remove } = cartSlice.actions;
+export default cartSlice.reducer;
