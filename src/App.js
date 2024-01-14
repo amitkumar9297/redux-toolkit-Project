de2c111cca8581ce7d0b2cca1381bd13f2517dd4
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-14 13:22:54 +0530

    intialize project and little bit changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
+// import './App.css';
+import './index.css'
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Navbar from './Components/Navbar';
+import store from './store/store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path='/' Component={Home} />
+            <Route path='/cart' Component={Cart} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
