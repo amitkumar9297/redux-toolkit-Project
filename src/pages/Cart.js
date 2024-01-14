dfe5dc9aaa18a374afe534b1d5fcf755ecf632fd
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-14 17:23:24 +0530

    cart page design ready remove product logic implemented

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { remove } from '../store/cartSlice'
 
 const Cart = () => {
+    const products = useSelector(state => state.cart);
+    const dispatch = useDispatch()
+    const handleRemove = (itemId) => {
+        dispatch(remove(itemId));
+    }
     return (
         <div>
-            cart
+            <h3>Cart</h3>
+            <div className='cartWrapper'>
+                {
+                    products.map(product => (
+                        <div className='cartCard'>
+                            <img src={product.image} alt='img' />
+                            <h5>{product.title}</h5>
+                            <h5>{product.price}</h5>
+                            <button onClick={() => handleRemove(product.id)} className='btn'>Remove</button>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
