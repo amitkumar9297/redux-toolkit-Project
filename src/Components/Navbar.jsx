ed75018fe15bc46762a80abb38e2e4417c707937
amitkumar9297 <amitkumar790194@gmail.com>
2024-01-14 13:21:08 +0530

    components of page

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <span className='logo'>STORE</span>
+            <div>
+                <Link className='navLink' to='/'>Home</Link>
+                <Link className='navLink' to={'/cart'}>Cart</Link>
+                <span className='cartCount'>
+                    cart Item : 0
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
